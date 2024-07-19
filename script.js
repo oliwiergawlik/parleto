a862ea9cca9ac7aa76cf123ba0373811c232bc05
oliwiergawlik <oliwier.gawlik0@gmail.com>
2024-07-19 13:43:29 +0200

    code cleanup

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 const expenses = {
     "2023-01": {
         "01": {
-            "food": [ 22.11, 43, 11.72, 2.2, 36.29, 2.5, 19 ],
-            "fuel": [ 210.22 ]
+            "food": [22.11, 43, 11.72, 2.2, 36.29, 2.5, 19],
+            "fuel": [210.22]
         },
         "09": {
-            "food": [ 11.9 ],
-            "fuel": [ 190.22 ]
+            "food": [11.9],
+            "fuel": [190.22]
         }
     },
     "2023-03": {
         "07": {
-            "food": [ 20, 11.9, 30.20, 11.9 ]
+            "food": [20, 11.9, 30.20, 11.9]
         },
         "04": {
-            "food": [ 10.20, 11.50, 2.5 ],
+            "food": [10.20, 11.50, 2.5],
             "fuel": []
         }
     },
     "2023-04": {}
 };
+
+console.log(solution(expenses));
+
+//----------------
 
 function getFirstSunday(year, month){
     const yearNum = parseInt(year);
@@ -50,10 +54,9 @@ function getMedian(numbers) {
    if (n % 2 === 0) {
        return (numbers[middle - 1] + numbers[middle]) / 2;
    } else {
-       return numbers[middle]
+       return numbers[middle];
    }
 }
-
 
 function solution(expenses){
     const result = {};
@@ -66,7 +69,7 @@ function solution(expenses){
 
         for (const day in expenses[date]) {
             if (Number(day) <= firstSunday) {
-                dateExpenses = dateExpenses.concat(getExpenses(expenses[date][day]))
+                dateExpenses = dateExpenses.concat(getExpenses(expenses[date][day]));
             }
         }
 
@@ -75,5 +78,3 @@ function solution(expenses){
     
     return result;
 }
-
-console.log(solution(expenses))
